04b3fdb2e19d9ba1d1faa2b6e654853fd8761249
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-09 17:52:53 -0300

    insert logo

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,22 +1,25 @@
 import React from 'react'; 
-import pic from "../assets/chocolate.png"
+import pic from "../assets/chocolate.png";
+import logo from "../assets/logo.png"
 
 const Home = () => {
   return (
-    <div className='w-full mx-auto'>
-   <div className="font-extrabold  my-10 font-sans text-[#531e0d]">
-  <h1 className="inline text-6xl text-shadow-sm">m</h1>
-  <p className="inline text-2xl">&</p>
-  <h1 className="inline text-6xl text-shadow-sm">m</h1>
-  <p className="inline text-2xl">'s</p>
-</div>
-<div className='flex flex-col lg:flex-row justify-center items-center mx-auto w-full my-16 border-2  bg-[#f6d538] border-slate-100 shadow-md py-4'>
-<div className='flex lg:justify-end justify-center items-center mx-auto my-24 w-full lg:w-6/12' >
-  <img src={pic} alt=''/>
-</div>
-</div>
+ 
+<div className="absolute inset-0">
+  <img className="object-cover w-full h-full" src="https://i.imgur.com/1me1qZr.jpg" alt="Descrição da imagem"/>
+  <div className="absolute inset-0 flex flex-col justify-center items-center">
+   <img className='h-32'src={logo} alt= ""/>
+    <div className="flex flex-col lg:flex-row justify-center items-center mx-auto w-full my-16 border-2 bg-[#f6d538] border-slate-400 shadow-md py-4">
+      <div className="flex lg:justify-end justify-center items-center mx-auto my-24 w-full lg:w-6/12">
+        <img src={pic} alt=""/>
+      </div>
+      <div className="flex flex-col lg:w-6/12 w-full py-8">
+      </div>
     </div>
+  </div>
+</div>
+
   )
 }
 
-export default Home
+export default Home;
