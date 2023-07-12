1d10eccbc42f561ba2dd24d512074b9ab68e67ad
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-12 13:41:43 -0300

    create cancel

--- frontend/src/components/Cancel.jsx
+++ frontend/src/components/Cancel.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import cancel from "../assets/cancel.png";
+import {Link} from "react-router-dom";
 
 const Cancel = () => {
   return (
-    <div>Cancel</div>
+    <div className='grid place-items-center w-full lg:h-screen font-raleway' style={{backgroundImage: 'url(https://i.imgur.com/nzPoHw2.png)', backgroundSize:'cover',backgroundPosition: 'center' }}>
+      <div className='max-w-6xl rounded flex flex-col'>
+      <span className='text-red-600 text-5xl font-bold'>Algo deu errado!!</span>
+      <span className='text-orange-500 text-center mt-8 text-2xl font-semibold'>
+          Tente mais tarde.
+        </span>
+        <div className='flex justify-end items-center mx-auto my-12 w-96'>
+          <img src={cancel} alt=""/>
+        </div>
+<div className='my-10 mx-auto'>
+<Link to="/" className='underline text-xl underline-offset-4'>Voltar</Link>
+</div>
+      </div>
+    </div>
   )
 }
 
-export default Cancel
+export default Cancel;
